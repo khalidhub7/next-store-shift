3fa460d02ea6773069484bd70e897df140888c5d
khalidhub7 <louguikhalid7@gmail.com>
2026-02-15 04:50:29 +0100

    task

---
+++ app/about/page.tsx
@@ -0,0 +1,17 @@
+const About = () => (
+  <section>
+    <header>
+      <h3>About page</h3>
+    </header>
+    <body>
+      <p>
+        We are an online store that sells high-quality electronic products like
+        phones, laptops, accessories, and smart devices. Our goal is to offer
+        reliable products at fair prices with fast delivery and friendly
+        support. We focus on quality, simplicity, and customer satisfaction.
+      </p>
+    </body>
+  </section>
+);
+
+export default About;
