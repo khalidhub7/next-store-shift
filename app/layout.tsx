65a451bc98c90cc8973f4d52c33a1e5610e66c54
khalidhub7 <louguikhalid7@gmail.com>
2026-02-15 21:12:50 +0100

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,25 @@
 import "./globals.css";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button"
+import Link from "next/link";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body className="">
-        <header>
+        <header className="flex h-14 border border-gray-200 items-center justify-between px-4">
           <Avatar>
             <AvatarImage src="https://github.com/shadcn.png" />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
+          {/* nav */}
+
+          <Button>
+            <Link>
+            
+            </Link>
+          </Button>
+          
         </header>
         <main>{children}</main>
 
