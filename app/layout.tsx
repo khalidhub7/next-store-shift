b4ee47d8d92f84d0a77aa86766af029a3c763ea4
khalidhub7 <louguikhalid7@gmail.com>
2026-02-15 06:00:47 +0100

    add comp

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,20 @@
 import "./globals.css";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body className="">{children}</body>
+      <body className="">
+        <header>
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </header>
+        <main>{children}</main>
+
+        <footer></footer>
+      </body>
     </html>
   );
 };
