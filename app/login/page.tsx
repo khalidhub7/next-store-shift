7109f2a12eb20c9656b29d80fc54b9432e8269cc
khalidhub7 <louguikhalid7@gmail.com>
2026-02-17 19:17:43 +0100

    fix Operator precedence

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,10 @@
-const Auth = () => {
-  // we can say that is user LoggedIn or session is experied
-  const isLoggedIn = Math.random() < 0.5;
+const Auth = async ({ searchParams }: any) => {
+  const { auth } = await searchParams;
+  const isLoggedIn = auth === "true";
 
   return (
     <div className="p-24 text-center text-fuchsia-500 ">
-        <p> {isLoggedIn ? "You are logged in" : "You are not logged in"} </p>
+      <p>{isLoggedIn ? "You are logged in" : "You are not logged in"}</p>
     </div>
   );
 };
