10d6bc9077f2249c0e2d6ce73e068098637ab181
khalidhub7 <louguikhalid7@gmail.com>
2026-02-20 23:12:51 +0100

    handle status

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,15 @@
+import { Badge } from "@/components/ui/badge";
+
 const Auth = async ({ searchParams }: any) => {
   const { auth } = await searchParams;
   const isLoggedIn = auth === "true";
 
   return (
     <div className=" h-96 text-fuchsia-500 flex items-center justify-center font-mono ">
-      <p>{isLoggedIn ? "You are logged in" : "You are not logged in"}</p>
+      auth status:{" "}
+      <Badge variant="destructive">
+        {isLoggedIn ? "logged in" : "logged out"}
+      </Badge>
     </div>
   );
 };
--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Home = () => (
   <section className="md:max-w-4/5 mx-auto mt-10 p-12 rounded-lg shadow-md text-center flex flex-col gap-10">
-    <h1 className="text-4xl font-medium text-fuchsia-500" >electroNext</h1>
+    <h1 className="text-4xl font-medium text-fuchsia-500">electroNext</h1>
 
     <p>
       Your one-stop shop for modern electronic products. Discover laptops,
