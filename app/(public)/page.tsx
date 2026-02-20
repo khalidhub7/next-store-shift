20afd17e975d1adaec153cbab78cdac7c08af9d8
khalidhub7 <louguikhalid7@gmail.com>
2026-02-20 19:24:48 +0100

    responsive

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const Home = () => (
-  <section className="md:max-w-4/5 mx-auto mt-10 p-6 rounded-lg shadow-md text-center flex flex-col gap-8">
+  <section className="md:max-w-4/5 mx-auto mt-10 p-12 rounded-lg shadow-md text-center flex flex-col gap-10">
     <h1>electroNext</h1>
 
     <p>
