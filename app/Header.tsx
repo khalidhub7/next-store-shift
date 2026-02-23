bdcc8230df0491aa2d9c4727120de7ad12623fcf
khalidhub7 <louguikhalid7@gmail.com>
2026-02-23 22:52:42 +0100

    up

--- app/Header.tsx
+++ app/Header.tsx
@@ -14,22 +14,22 @@ const Header = () => (
     <nav className="">
       <ul className="flex w-3/4 justify-end gap-4">
         <li>
-          <Button asChild size="sm" variant="outline">
+          <Button asChild size="lg" variant="outline">
             <Link href="/">home</Link>
           </Button>
         </li>
         <li>
-          <Button asChild size="sm" variant="outline">
+          <Button asChild size="lg" variant="outline">
             <Link href="/login">sign(in/up)</Link>
           </Button>
         </li>
         <li>
-          <Button asChild size="sm" variant="outline">
+          <Button asChild size="lg" variant="outline">
             <Link href="/about">about</Link>
           </Button>
         </li>
         <li>
-          <Button asChild size="sm" variant="outline">
+          <Button asChild size="lg" variant="outline">
             <Link href="/products">products</Link>
           </Button>
         </li>
