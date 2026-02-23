7d1bf0a0fa1af455610522ed3a4e35628cfd1f94
khalidhub7 <louguikhalid7@gmail.com>
2026-02-23 22:55:34 +0100

    up

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 
 const Home = () => (
   <section className="md:max-w-4/5 mx-auto mt-10 p-12 rounded-lg shadow-md text-center flex flex-col gap-10">
-    <h1 className="text-4xl font-medium text-fuchsia-500">GlowEssence</h1>
+    <h1 className="text-4xl font-medium text-fuchsia-500 hover:scale-105 transition-transform duration-300">
+      GlowEssence
+    </h1>
 
     <p>
       Elevate your beauty routine with everyday makeup essentials. From bold
