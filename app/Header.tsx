172a1092cc6cf0ea4036d14c8a84890dcbdea238
khalidhub7 <louguikhalid7@gmail.com>
2026-02-23 22:53:44 +0100

    up

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,7 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 const Header = () => (
   <header className="bg-gray-50 flex h-14 border border-gray-200 items-center justify-between px-4">
     {/* avatar */}
-    <Avatar size="lg">
+    <Avatar size="default">
       <AvatarImage src="https://github.com/shadcn.png" />
       <AvatarFallback>avatar</AvatarFallback>
     </Avatar>
