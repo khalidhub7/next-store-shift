d99f7ecbc895a3e1e893a073ea69d30cb88b7264
khalidhub7 <louguikhalid7@gmail.com>
2026-03-02 09:03:43 +0100

    clean structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Header from "./Header";
+import Header from "./components/Header";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
