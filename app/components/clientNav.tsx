9035ab40a2b16549d3a7e7d6d7d83275761698fa
khalidhub7 <louguikhalid7@gmail.com>
2026-03-02 09:53:52 +0100

    add active state

--- app/components/clientNav.tsx
+++ app/components/clientNav.tsx
@@ -1,28 +1,51 @@
-import { Button } from "@/components/ui/button";
+"use client";
 import Link from "next/link";
+import styles from "./Header.module.css";
 import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 const ClientNav = () => {
+  const currentPath = usePathname();
   return (
     <nav className="">
       <ul className="flex w-3/4 justify-end gap-4">
         <li>
-          <Button asChild size="lg" variant="outline">
+          <Button
+            asChild
+            size="lg"
+            variant="outline"
+            className={currentPath === "/" ? styles.active : undefined}
+          >
             <Link href="/">home</Link>
           </Button>
         </li>
         <li>
-          <Button asChild size="lg" variant="outline">
+          <Button
+            asChild
+            size="lg"
+            variant="outline"
+            className={currentPath === "/login" ? styles.active : undefined}
+          >
             <Link href="/login">sign(in/up)</Link>
           </Button>
         </li>
         <li>
-          <Button asChild size="lg" variant="outline">
+          <Button
+            asChild
+            size="lg"
+            variant="outline"
+            className={currentPath === "/about" ? styles.active : undefined}
+          >
             <Link href="/about">about</Link>
           </Button>
         </li>
         <li>
-          <Button asChild size="lg" variant="outline">
+          <Button
+            asChild
+            size="lg"
+            variant="outline"
+            className={currentPath === "/products" ? styles.active : undefined}
+          >
             <Link href="/products">products</Link>
           </Button>
         </li>
