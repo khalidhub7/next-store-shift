960332b33a2ca2c2f262aac88203bbde09288335
khalidhub7 <louguikhalid7@gmail.com>
2026-03-02 09:23:01 +0100

    isolate nav

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,4 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
+import ClientNav from "./clientNav";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Header = () => (
@@ -9,32 +8,9 @@ const Header = () => (
       <AvatarImage src="https://github.com/shadcn.png" />
       <AvatarFallback>avatar</AvatarFallback>
     </Avatar>
-    {/* nav */}
+    {/* client nav */}
 
-    <nav className="">
-      <ul className="flex w-3/4 justify-end gap-4">
-        <li>
-          <Button asChild size="lg" variant="outline">
-            <Link href="/">home</Link>
-          </Button>
-        </li>
-        <li>
-          <Button asChild size="lg" variant="outline">
-            <Link href="/login">sign(in/up)</Link>
-          </Button>
-        </li>
-        <li>
-          <Button asChild size="lg" variant="outline">
-            <Link href="/about">about</Link>
-          </Button>
-        </li>
-        <li>
-          <Button asChild size="lg" variant="outline">
-            <Link href="/products">products</Link>
-          </Button>
-        </li>
-      </ul>
-    </nav>
+    <ClientNav />
   </header>
 );
 
--- app/components/clientNav.tsx
+++ app/components/clientNav.tsx
@@ -1,9 +1,34 @@
-import { usePathname } from "next/navigation"
-
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const ClientNav = () => {
+  return (
+    <nav className="">
+      <ul className="flex w-3/4 justify-end gap-4">
+        <li>
+          <Button asChild size="lg" variant="outline">
+            <Link href="/">home</Link>
+          </Button>
+        </li>
+        <li>
+          <Button asChild size="lg" variant="outline">
+            <Link href="/login">sign(in/up)</Link>
+          </Button>
+        </li>
+        <li>
+          <Button asChild size="lg" variant="outline">
+            <Link href="/about">about</Link>
+          </Button>
+        </li>
+        <li>
+          <Button asChild size="lg" variant="outline">
+            <Link href="/products">products</Link>
+          </Button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-}
-
-
-export default ClientNav
+export default ClientNav;
