78d263546a888070d59e337c1ff617cb4545612f
khalidhub7 <louguikhalid7@gmail.com>
2026-03-03 09:26:32 +0100

    ux logic fix

--- app/components/clientNav.tsx
+++ app/components/clientNav.tsx
@@ -17,7 +17,8 @@ const ClientNav = () => {
     <nav className="">
       <ul className="flex w-3/4 justify-end gap-4">
         {links.map(({ href, label }) => {
-          const isActive = currentPath.startsWith(href);
+          const isActive =
+            href === "/" ? currentPath === "/" : currentPath.startsWith(href);
 
           return (
             <li key={href}>
