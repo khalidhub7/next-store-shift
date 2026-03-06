b0372a9fb5b37952c85209440366e8c585a5e501
khalidhub7 <louguikhalid7@gmail.com>
2026-03-06 08:57:42 +0100

    use clean loading

--- app/(shop)/products/loading.tsx
+++ app/(shop)/products/loading.tsx
@@ -1,8 +1,10 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 const Loading = () => {
   return (
-    <section className="max-w-3xl mx-auto mt-10 p-6 rounded-lg shadow-md text-center flex flex-col gap-8 animate-pulse">
+    <section className="max-w-3xl mx-auto mt-10 p-6 rounded-lg shadow-md text-center flex flex-col gap-8">
       <header>
-        <div className="h-6 w-32 bg-gray-100 rounded mx-auto" />
+        <Skeleton className="h-6 w-32 mx-auto" />
       </header>
 
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto">
@@ -12,11 +14,11 @@ const Loading = () => {
             className="border rounded shadow p-4 flex flex-col gap-4 w-sm md:w-full"
           >
             <div className="flex items-center justify-between">
-              <div className="h-4 w-24 bg-gray-100 rounded" />
-              <div className="h-4 w-10 bg-gray-100 rounded" />
+              <Skeleton className="h-4 w-24" />
+              <Skeleton className="h-4 w-10" />
             </div>
 
-            <div className="w-full aspect-[16/12] bg-gray-100 rounded" />
+            <Skeleton className="w-full aspect-[16/12] rounded" />
           </li>
         ))}
       </ul>
