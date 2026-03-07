22cb851f168977a188d1522759b95e07e89c6809
khalidhub7 <louguikhalid7@gmail.com>
2026-03-07 09:39:23 +0100

    handle Server Actions

--- app/actions/cart.ts
+++ app/actions/cart.ts
@@ -20,14 +20,17 @@ const reloadCart = (): Array<CartItem> => {
 };
 
 const addToCart = (product: Product) => {
-  let newCart;
+  let newCart: Array<CartItem>;
   const cart = reloadCart();
   const isExist = cart.find((i) => i.id === product.id);
 
   if (isExist) {
-    newCart = cart.map((p) =>
-      p.id === product.id ? { ...product, qty: product.qty + 1 } : p,
-    );
+    newCart = cart.map((p) => {
+      const { id, title, price, qty } = isExist;
+      return p.id === id
+        ? { id, title, price, qty: qty + 1 }
+        : { id, title, price, qty };
+    });
   } else {
     const { id, title, price } = product;
     newCart = [...cart, { id, title, price, qty: 1 }];
