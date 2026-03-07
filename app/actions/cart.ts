6e0cf3c6062f43e64a084246b8b9d86a948fb3d5
khalidhub7 <louguikhalid7@gmail.com>
2026-03-07 09:59:10 +0100

    handle Server Actions

--- app/actions/cart.ts
+++ app/actions/cart.ts
@@ -12,9 +12,13 @@ interface CartItem {
 
 // interface Cart {}
 
-const appCookies = await cookies();
+/*  todo:
+read and understand what happen 
+then handle appCookies agent with best practices
+*/
 
-const reloadCart = (): Array<CartItem> => {
+const reloadCart = async (): Array<CartItem> => {
+  const appCookies = await cookies();
   const cart = appCookies.get("cart");
   return cart ? JSON.parse(cart.value) : [];
 };
@@ -27,9 +31,7 @@ const addToCart = (product: Product) => {
   if (isExist) {
     newCart = cart.map((p) => {
       const { id, title, price, qty } = isExist;
-      return p.id === id
-        ? { id, title, price, qty: qty + 1 }
-        : { id, title, price, qty };
+      return p.id === id ? { id, title, price, qty: qty + 1 } : p;
     });
   } else {
     const { id, title, price } = product;
