508b1513061f183ee302ce993724aa45665241a5
khalidhub7 <louguikhalid7@gmail.com>
2026-03-09 07:47:48 +0100

    avoid race conditions

--- app/actions/cart.ts
+++ app/actions/cart.ts
@@ -9,27 +9,47 @@ interface CartItem {
   price: number;
   qty: number;
 }
+type Task = () => Promise<void>;
+
+// help to avoid race conditions
+let resolveActionsQueue = Promise.resolve();
+const appendToQueue = async (task: Task) => {
+  resolveActionsQueue = resolveActionsQueue
+    .then(() => task())
+    .catch(() => undefined);
+  return resolveActionsQueue;
+};
 
 const reloadCart = async () => {
   // return the appCookies + cart
   const appCookies = await cookies();
-  const request = appCookies.get("cart")
-  const cart = request ? JSON.parse(request.value) : []
-  return {cart, appCookies}
+  const request = appCookies.get("cart");
+  const cart = request ? JSON.parse(request.value) : [];
+  return { cart, appCookies };
 };
 
-const addToCart = (product: Product) => {
-  let newCart: Array<CartItem>;
-  const cart = reloadCart();
-  const isExist = cart.find((i) => i.id === product.id);
+const addToCart = async (product: Product) => {
+  const task = async () => {
+    let newCart: Array<CartItem>;
+    const { appCookies, cart } = await reloadCart();
+    const isExist = cart.find((i: CartItem) => i.id === product.id);
 
-  if (isExist) {
-    newCart = cart.map((p) => {
-      const { id, title, price, qty } = isExist;
-      return p.id === id ? { id, title, price, qty: qty + 1 } : p;
+    if (isExist) {
+      newCart = cart.map((p: CartItem) => {
+        const { id, title, price, qty } = isExist;
+        return p.id === id ? { id, title, price, qty: qty + 1 } : p;
+      });
+    } else {
+      const { id, title, price } = product;
+      newCart = [...cart, { id, title, price, qty: 1 }];
+    }
+
+    appCookies.set("cart", JSON.stringify(newCart), {
+      httpOnly: false,
+      path: "/",
+      maxAge: undefined,
     });
-  } else {
-    const { id, title, price } = product;
-    newCart = [...cart, { id, title, price, qty: 1 }];
-  }
+  };
+
+  return appendToQueue(task);
 };
