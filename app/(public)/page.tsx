bc31859252925923dca60cdba3251d70353300a8
khalidhub7 <louguikhalid7@gmail.com>
2026-03-24 15:25:58 +0100

    keep debugging

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,5 +1,6 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 
 const Home = () => (
   <section className="md:max-w-4/5 mx-auto mt-10 p-12 rounded-lg shadow-md text-center flex flex-col gap-10">
