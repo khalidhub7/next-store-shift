9270ad1c642701f3631ba121494c1b919006f868
khalidhub7 <louguikhalid7@gmail.com>
2026-03-25 15:26:27 +0100

    style

--- app/(shop)/products/page.tsx
+++ app/(shop)/products/page.tsx
@@ -14,14 +14,11 @@ const Products = async () => {
 
   return (
     <section className="max-w-6xl mx-auto mt-16 px-6 flex flex-col gap-10">
-      
       <header className="text-center flex flex-col gap-2">
         <span className="text-xs font-semibold tracking-widest uppercase text-muted-foreground">
           Our Collection
         </span>
-        <h1 className="text-3xl font-bold text-gray-900">
-          Products
-        </h1>
+        <h1 className="text-3xl font-bold text-gray-900">Products</h1>
       </header>
 
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -29,7 +26,6 @@ const Products = async () => {
           <ProductCard p={p} key={p.id} />
         ))}
       </ul>
-
     </section>
   );
 };
