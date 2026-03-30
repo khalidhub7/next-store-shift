03c2e43c2b94c088b6c951ce2f5f9cf43b54324b
khalidhub7 <louguikhalid7@gmail.com>
2026-03-30 15:46:09 +0200

    next mission (auth)

--- lib/auth/session.ts
+++ lib/auth/session.ts
@@ -1,16 +1,20 @@
 import { randomUUID } from "crypto";
 
 const CreateSession = (userId: string) => {
+  // session
   const createdAt = new Date();
-
   const session = {
-    session: {
-      id: randomUUID(),
+    sessionId: randomUUID(),
+    data: {
       userId,
       createdAt: createdAt.toISOString(),
       expiresAt: new Date(
-        createdAt.getTime() + 1000 * 60 * 60 * 24 * 3,
+        createdAt.getTime() + 1000 * 60 * 60 * 24 * 3, // 3 days
       ).toISOString(),
     },
   };
+
+  // store session reference (cookies)
+
+  // store session in db
 };
--- lib/db.ts
+++ lib/db.ts
@@ -6,36 +6,31 @@ data/*.json = source of truth
 db.ts = reads/writes files
 
 Later (real DB)
-DELETE data/
+DELETE data/ folder
 db.ts → connects to real DB
 */
 
-import { promises as fs } from "fs";
 import path from "path";
+import { promises as fs } from "fs";
 import { randomUUID } from "crypto";
-
 const filePath = path.join(process.cwd(), "lib/data/carts.json");
 
+// cart crud helpers
 const getCarts = async () => {
   const data = await fs.readFile(filePath, "utf-8");
   return data === "" ? [] : JSON.parse(data);
 };
-
 const saveCarts = async (carts: any[]) => {
   await fs.writeFile(filePath, JSON.stringify(carts, null, 2));
 };
 
-// get cart
+// cart crud
 const getCartById = async (id: string) => {
   const carts = await getCarts();
   return carts.find((c: any) => c.id === id)
 };
-
-// create cart → returns cartId
 const createCart = async (items: any[]) => {
-  
   const carts = await getCarts();
-
   const newCart = {
     id: randomUUID(),
     userId: "unknown",
@@ -43,17 +38,12 @@ const createCart = async (items: any[]) => {
     createdAt: new Date().toISOString(),
     updatedAt: new Date().toISOString(),
   };
-
   carts.push(newCart);
   await saveCarts(carts);
-
   return newCart.id;
 };
-
-// update cart (replace items)
 const updateCart = async (id: string, items: any[]) => {
   const carts = await getCarts();
-
   const newCarts = carts.map((c: any) =>
     c.id === id
       ? {
@@ -63,15 +53,16 @@ const updateCart = async (id: string, items: any[]) => {
         }
       : c,
   );
-
   await saveCarts(newCarts);
 };
-
-// delete cart
 const deleteCart = async (id: string) => {
   const carts = await getCarts();
   const newCarts = carts.filter((c: any) => c.id !== id);
   await saveCarts(newCarts);
 };
+
+// session crud
+
+
 
 export { createCart, updateCart, deleteCart, getCartById };
