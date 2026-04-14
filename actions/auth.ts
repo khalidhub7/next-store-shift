4a121b096c81baf1f1875d1f82fc328e9b1e223f
khalidhub7 <louguikhalid7@gmail.com>
2026-04-15 00:02:44 +0100

    test auth

--- actions/auth.ts
+++ actions/auth.ts
@@ -12,7 +12,8 @@ const cookieOptions: Parameters<Awaited<ReturnType<typeof cookies>>["set"]>[2] =
     httpOnly: true,
     secure: true,
     sameSite: "lax",
-    maxAge: 60 * 60 * 24 * 3,
+    // maxAge: 60 * 60 * 24 * 3,
+    maxAge: 60 
   };
 
 const loginAction = async (data: LoginData) => {
