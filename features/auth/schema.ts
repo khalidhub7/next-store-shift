03f49bb11f033d592f26d5c5063bcab0d976b1a6
khalidhub7 <louguikhalid7@gmail.com>
2026-04-23 10:10:09 +0100

    improve auth

--- features/auth/schema.ts
+++ features/auth/schema.ts
@@ -1,15 +1,15 @@
 import { z } from "zod";
 
 const loginSchema = z.object({
-  email: z.string().email("invalid email"),
-  password: z.string().min(4),
+  email: z.string().email("invalid email").trim().toLowerCase(),
+  password: z.string().min(8).trim(),
 });
 const registerSchema = z
   .object({
     name: z.string().min(5),
-    email: z.string().email("invalid email"),
-    password: z.string().min(4),
-    confirmPassword: z.string().min(4),
+    email: z.string().email("invalid email").trim().toLowerCase(),
+    password: z.string().min(8).trim(),
+    confirmPassword: z.string().min(8).trim(),
   })
   .refine((obj) => obj.password === obj.confirmPassword, {
     message: "Passwords must match",
