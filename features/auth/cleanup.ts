349988535d2c66fe799931ea916908edd3a8564b
khalidhub7 <louguikhalid7@gmail.com>
2026-06-09 06:38:55 +0100

    implement sessions cleanup

--- features/auth/cleanup.ts
+++ features/auth/cleanup.ts
@@ -11,9 +11,11 @@ import { readdir } from "fs/promises";
 import { getSession } from "./server";
 import { deleteSession } from "./db/session";
 
+const SESSION_TTL_MS = 30 * 24 * 60 * 60 * 1000; // 30 days
 const sessionsDir = path.join(process.cwd(), "storage", "auth", "sessions");
 
 const cleanupSessions = async () => {
+  // When moving to MySQL, you'll replace that part with a DB query.
   const sessions = await readdir(sessionsDir);
 
   for (const session of sessions) {
@@ -22,8 +24,6 @@ const cleanupSessions = async () => {
     const sessionObj = await getSession(sessionId);
 
     if (!sessionObj) continue;
-
-    const SESSION_TTL_MS = 30 * 24 * 60 * 60 * 1000; // 30 days
 
     const oldRevoked =
       sessionObj.revokedAt &&
